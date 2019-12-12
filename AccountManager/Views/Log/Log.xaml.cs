17e42d378c554d862672c78a88248ee347b8f5f7
yvan vander sanden <yvan@attr-x.net>
2019-12-12 12:44:20 +0100

    re-added log

---
+++ AccountManager/Views/Log/Log.xaml.cs
@@ -0,0 +1,141 @@
+using AccountApi;
+using AccountManager.Views.Log;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AccountManager.Views.Log
+{
+    /// <summary>
+    /// Interaction logic for Log.xaml
+    /// </summary>
+    public partial class Log : UserControl, ILog
+    {
+        public bool ShowErrors { set; get; } = true;
+        public bool ShowMessages { set; get; } = true;
+
+        List<LogEntry> entries = new List<LogEntry>();
+        ObservableCollection<LogEntry> VisibleEntries = new ObservableCollection<LogEntry>();
+
+        public Origin Filter { get; set; } = Origin.All;
+
+        public Log()
+        {
+            InitializeComponent();
+            LogList.ItemsSource = VisibleEntries;
+        }
+
+        private void ErrorsButton_Click(object sender, RoutedEventArgs e)
+        {
+            ShowErrors = !ShowErrors;
+            Rebuild();
+
+            if (ShowErrors)
+            {
+                ErrorsButton.Style = this.FindResource("MaterialDesignRaisedButton") as Style;
+            }
+            else
+            {
+                ErrorsButton.Style = this.FindResource("MaterialDesignFlatButton") as Style;
+            }
+
+        }
+
+        private void WarningsButton_Click(object sender, RoutedEventArgs e)
+        {
+            ShowMessages = !ShowMessages;
+            Rebuild();
+
+            if (ShowMessages)
+            {
+                WarningsButton.Style = this.FindResource("MaterialDesignRaisedButton") as Style;
+            }
+            else
+            {
+                WarningsButton.Style = this.FindResource("MaterialDesignFlatButton") as Style;
+            }
+        }
+
+        private void Rebuild()
+        {
+            VisibleEntries.Clear();
+            int errors = 0;
+            int messages = 0;
+
+            for (int i = entries.Count - 1; i >= 0; i--)
+            {
+                if (entries[i].Error)
+                {
+                    errors++;
+                }
+                else
+                {
+                    messages++;
+                }
+
+                if (entries[i].Error && !ShowErrors) continue;
+                if (!entries[i].Error && !ShowMessages) continue;
+
+                if (Filter == Origin.All || Filter == entries[i].Origin)
+                {
+                    VisibleEntries.Add(entries[i]);
+                }
+            }
+            VisibleEntries.Reverse();
+            MessagesBadge.Badge = messages.ToString();
+            ErrorsBadge.Badge = errors.ToString();
+        }
+
+        public void AddMessage(Origin origin, string message)
+        {
+            entries.Add(new LogEntry
+            {
+                Origin = origin,
+                Content = message,
+                Error = false,
+            });
+            Application.Current.Dispatcher.Invoke(new System.Action(() => Rebuild()));
+        }
+
+        public void AddError(Origin origin, string message)
+        {
+            entries.Add(new LogEntry
+            {
+                Origin = origin,
+                Content = message,
+                Error = true,
+            });
+            Application.Current.Dispatcher.Invoke(new System.Action(() => Rebuild()));
+        }
+
+        private void OriginSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selected = (OriginSelector.SelectedValue as ComboBoxItem).Content?.ToString();
+            if (selected == null) return;
+
+            switch (selected)
+            {
+                case "All": Filter = Origin.All; break;
+                case "Wisa": Filter = Origin.Wisa; break;
+                case "SmartSchool": Filter = Origin.Smartschool; break;
+                case "Active Directory": Filter = Origin.Directory; break;
+                case "Other": Filter = Origin.Other; break;
+            }
+
+            Rebuild();
+        }
+    }
+}
